d5263d1dcd0e50e1b6fadd5105b382bc31629d82
jrpompio <junior.ruiz@ucr.ac.cr>
2024-03-18 03:48:22 -0600

    Agregando previo1 y archivos de prueba c++

---
+++ Previos/Previo1/compatiblidadC.cpp
@@ -0,0 +1,9 @@
+ // Probando compatibilidad con C
+ #include <stdio.h>
+  
+ int main() { 
+    
+    printf("Comprobando compatiblidad con comandos de C");
+    return 0;
+ }
+
---
+++ Previos/Previo1/primerCodigo.cpp
@@ -0,0 +1,7 @@
+// Primer codigo realizado en c++
+#include <iostream>
+
+int main(){
+    std::cout << "Hola mundo" << std::endl; 
+    return 0;
+}
