67cbe582c7bad2c630d0c88647fb2dfa39e7d876
jrpompio <junior.ruiz@ucr.ac.cr>
2024-03-19 23:49:06 -0600

    agregando uso de arg para argumentos de terminal
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

---
+++ Previos/Previo2/12-argumento_terminal.cpp
@@ -0,0 +1,13 @@
+// FUNCION VOID
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+
+    cout << "Número de argumentos: " << argc <<endl;
+    cout << "Nombre del programa: " << argv[0] <<endl;
+    
+    return 0;
+}
