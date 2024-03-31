c8902270e0f1356aaa98f2b2319cc7067333d542
jrpompio <junior.ruiz@ucr.ac.cr>
2024-03-31 06:19:58 -0600

    Agregando archivo correspondiente a funciones que reciben objetos como parametros de entrada : Previo 4
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

---
+++ Previos/Previo4/3-objectToClass.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    public:
+        double marks;
+    Student(double m){
+        marks = m;
+    }
+};
+
+void calculateAverage(Student s1, Student s2){
+
+    double average = (
+        s1.marks           // Acá la función accede al atributo marks 
+      + s2.marks)/2;       // de cada objeto de clase Student que recibió
+        
+
+    cout << "Average Marks = "
+     << average << endl;
+}
+
+int main() {
+    Student student1(88.0), student2(56.0);
+
+    // Uso de la función que recibe esta clase creada como parametro
+    calculateAverage(student1, student2); 
+    
+return 0;
+}
