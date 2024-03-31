ab3cfe513f099da2f82e8e2b2596ceb36aa24f3a
jrpompio <junior.ruiz@ucr.ac.cr>
2024-03-31 03:15:48 -0600

    Agregando archivo correspondiente a la primera clase creada en el curso
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

---
+++ Previos/Previo4/1-classObject.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class Molde {               // Class ClassName
+    public:                 // Especificador de accesor: Publico 
+        double largo;       // Elementos de datos de la clase
+        double ancho;
+        double alto;
+
+        // Creando constructor con introducción de valores
+        // para los elementos de datos de la clase
+        Molde(double largo_p, double ancho_p, double alto_p ) : 
+            largo(largo_p), ancho(ancho_p), alto(alto_p) {}
+
+        double calcularArea(){
+            return largo * ancho;
+        };
+
+        double calcularVolumen(){
+            return largo * ancho * alto;
+        };
+
+        ~Molde (){
+            cout << "Ya no soy necesario. Chao, cariño, chao" <<endl;
+        }
+
+};
+
+int main(){
+
+// Usando clase para crear un objeto pared introduciendo los valores deseados
+// para cada elemento de datos de la clase
+Molde pared(10.0, 20.0, 35.0);
+
+    // cout << pared.largo << endl;    // Mostrando valor pared.largo default 
+    // pared.largo = 20.0;
+    // pared.ancho = 13.2;
+    // pared.alto = 23.4;
+    // cout << pared.largo << endl << endl ;  // Mostrando valor pared.largo introducido
+
+    // Mostrando valor del area con el metodo calcularArea de la clase Molde
+    cout << "El area es: " << pared.calcularArea() << endl;  
+    // Mostrando valor del area con el metodo calcularVolumen de la clase Molde
+    cout << "El volumen es: " << pared.calcularVolumen() << endl;  
+
+
+
+return 0;    
+}
