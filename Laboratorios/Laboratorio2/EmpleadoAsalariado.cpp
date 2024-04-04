c0aa5bd74f05693d0e0a40b24784b02bac0ee44f
jrpompio <junior.ruiz@ucr.ac.cr>
2024-04-04 08:04:33 -0600

    Agregando archivos en receso
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

---
+++ Laboratorios/Laboratorio2/EmpleadoAsalariado.cpp
@@ -0,0 +1,10 @@
+#include "EmpleadoAsalariado.hpp"
+
+EmpleadoAsalariado::EmpleadoAsalariado(std::string _nombre, int _edad,  double _salario)
+: Empleado (_nombre, _edad, _salario) {}
+
+double EmpleadoAsalariado::calcularPago() const {
+    return salario;
+};
+
+
---
+++ Laboratorios/Laboratorio2/EmpleadoAsalariado.hpp
@@ -0,0 +1,12 @@
+#ifndef EMPLEADOASALARIADO_HPP
+#define EMPLEADOASALARIADO_HPP
+
+#include "Empleado.hpp"
+
+class EmpleadoAsalariado: public Empleado {
+  public:
+    EmpleadoAsalariado(std::string nombre, int _edad, double _salario);
+    double calcularPago() const override;
+};
+
+#endif
