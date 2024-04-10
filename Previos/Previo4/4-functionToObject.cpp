9339f458ace5ccc0a07f20df394602d6aff08255
jrpompio <junior.ruiz@ucr.ac.cr>
2024-04-10 17:25:15 -0600

    Cambios git ignore
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

--- Previos/Previo4/4-functionToObject.cpp
+++ Previos/Previo4/4-functionToObject.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+#define MAX_STUDENTS 20
+
 class Student {                 
   public:
     double marks1, marks2;
@@ -19,13 +21,14 @@ Student createStudent(){     // Creando función de tipo Student
 }
 
 int main() {
-  Student estudianteIE0217;
+  Student estudiantes[MAX_STUDENTS];
+  estudiantes[0] = createStudent();
 
-  estudianteIE0217 = createStudent();
-  // Mostrando datos obtenidos de nuevo pero mediante la obtención de sus
-  // atributos 
-  cout << "Marks 1 = " << estudianteIE0217.marks1 << endl;
-  cout << "Marks 2 = " << estudianteIE0217.marks2 << endl;
+  // estudianteIE0217 = createStudent();
+  // // Mostrando datos obtenidos de nuevo pero mediante la obtención de sus
+  // // atributos 
+   cout << "Marks 1 = " << estudiantes[0].marks1 << endl;
+   cout << "Marks 2 = " << estudiantes[0].marks2 << endl;
 
 return 0;
 }
