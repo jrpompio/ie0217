a02a20552d1df28fc5faa87a44d19962a6534198
jrpompio <junior.ruiz@ucr.ac.cr>
2024-04-18 18:24:49 -0600

    Cambio de la función de hashing por hash con valor 31, ya que este numero primo genera menos colisiones
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

--- Tareas/Tarea3/scr/hash.cpp
+++ Tareas/Tarea3/scr/hash.cpp
@@ -10,7 +10,7 @@ HashTable::HashTable(int maxElements_){
 int HashTable::hashFunction(const std::string &texto)
 {   int hash = 0;
     for (char j : texto){
-        hash = hash + j;
+        hash = (hash * 31) + j;
     }
     cout << "\n" << hash <<endl;
     cout << "\n" << hash % maxElements <<endl;
