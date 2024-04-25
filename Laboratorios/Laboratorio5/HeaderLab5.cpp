bf7b09b1e8a52b1eaaf7d9851878ff8c010a85b8
jrpompio <junior.ruiz@ucr.ac.cr>
2024-04-25 02:57:10 -0600

    header listo
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

--- Laboratorios/Laboratorio5/HeaderLab5.cpp
+++ Laboratorios/Laboratorio5/HeaderLab5.cpp
@@ -2,6 +2,7 @@
 #define HEADERLAB5
 
 #include <string>
+#include <vector>
 
 class Libro {
   public:
@@ -12,6 +13,17 @@ class Libro {
     Libro(std::string t,
           std::string a,
           int anio);
+    bool operator<(const Libro& l) const;
 };
+
+class Biblioteca{
+  private:
+    std::vector<libro> libros;
+  public:
+    void agregarLibro(const Libro& libro);
+    void mostrarLibros();
+    void ordenarLibrosPorAnio();
+    
+}
 
 #endif
