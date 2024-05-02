8623868f9928f62b29bdd824e991c2eda7b20f38
jrpompio <junior.ruiz@ucr.ac.cr>
2024-05-02 07:37:38 -0600

    Agregando archivos antes de la pausa
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

--- Laboratorios/Laboratorio6/main.cpp
+++ Laboratorios/Laboratorio6/main.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 #include <stdexcept>
 
-template <typename T>
+template <class T>
 class Statistics {
   public:
     Statistics(const std::vector<T>& data) : data(data) {
@@ -35,7 +35,36 @@ class Statistics {
 
 int main(){
 
+try {
 
+  // Creando vector que contiene elementos int 
+  std::vector<int> intData = {1, 2, 3, 4, 5};
+
+  /* Usando template de la clase para enviar contenedor de elementos tipo int
+   */ 
+  Statistics<int> statsInt(intData);
+
+  std::cout << "Media de intData: " << statsInt.mean() << std::endl;
+  std::cout << "Desviación estandar de intData: "
+    << statsInt.standardDeviation() << std::endl;
+  
+  // Creando vector que contiene elementos double
+  std::vector<double> doubleData = {1.5, 2.5, 3.5, 4.5, 5.5};
+  
+  /* Usando template de la clase para enviar contenedor de elementos tipo
+   * double
+   */
+  Statistics<double> statsDouble(doubleData);
+
+  std::cout << "Media de doubleData: " << statsDouble.mean() << std::endl;
+  std::cout << "Desviación estandar de doubleData: "
+    << statsDouble.standardDeviation() << std::endl;
+
+
+} catch (const std::invalid_argument& e) {
+  std::cerr << "Excepcion encontrada: " << e.what() << std::endl;
+  
+}
 
 return 0;   
 }
